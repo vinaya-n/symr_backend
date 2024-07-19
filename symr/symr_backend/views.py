9cd499141c5200d9fe522f4449f69de191fb1907
Vinaya <vinaya.narayanan@gmail.com>
2024-07-19 12:22:54 +0530

    Added access key params to sts client initialization

--- symr/symr_backend/views.py
+++ symr/symr_backend/views.py
@@ -755,7 +755,13 @@ def upload_file(request):
   
   # Initialize AWS clients
   s3_client = boto3.client('s3', region_name=os.getenv('AWS_REGION'))
-  sts_client = boto3.client('sts', region_name=os.getenv('AWS_REGION'))    
+  #sts_client = boto3.client('sts', region_name=os.getenv('AWS_REGION'))    
+  # Initialize the STS client
+  sts_client = boto3.client('sts',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=region
+    )
     
   if request.method == 'POST':
     auth_header = request.META.get('HTTP_AUTHORIZATION')
